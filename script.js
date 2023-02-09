88725506b5bc1e4cf4d9d2af31b0ba11d3ba8607
Dav1Samu3l <davidev.front@gmail.com>
2023-02-08 23:13:16 -0300

    removed the css variables and added localStorage.

---
+++ script.js
@@ -0,0 +1,18 @@
+function submit() {
+
+
+    document.querySelector('input[type="submit"]').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const name = document.querySelector('input[name="name"]').value;
+        const email = document.querySelector('input[name="email"]').value;
+        const phone = document.querySelector('input[name="phone"]').value;
+        const interest = document.querySelector('select[name="interest"]').value;
+
+        localStorage.setItem('name', name);
+        localStorage.setItem('email', email);
+        localStorage.setItem('phone', phone);
+        localStorage.setItem('interest', interest);
+    });
+}
+submit()
